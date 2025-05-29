5499ca7cdd7b56fb799dc42c06ceb920825d61f7
noodle <nonomi2603@gmail.com>
2025-05-29 14:59:14 +0500

    #5 Add modle ReportDateHttpRequest for http requests

---
+++ ReportDateGateway/Models/ReportDateHttpRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ReportDateGateway.Models;
+
+public class ReportDateHttpRequest
+{
+    public int DayOfMonth { get; set; }
+    public string? Date { get; set; }
+    public bool? Adjust { get; set; }
+}
