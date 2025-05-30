293d670e95fe694db3bfe5764f56768a26049546
noodle <nonomi2603@gmail.com>
2025-05-30 13:34:23 +0500

    made swager available in production

--- ReportDateGateway/Program.cs
+++ ReportDateGateway/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddFluentValidationAutoValidation()
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
